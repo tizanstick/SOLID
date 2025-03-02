a24773ce37c64422b5954a1a2d035d62884f24e5
tizanstick <94346967+tizanstick@users.noreply.github.com>
2025-03-02 17:41:42 -0500

    f

--- L.php
+++ L.php
@@ -32,7 +32,7 @@ class Mercedes extends Coche {
 
 //Crear o definir una clase
 class ImprimirAsientos {
-    //Variables o atributos
+    //Variables o atributoss
     var $arrayCoches;
 
     // Funciones o métodos
